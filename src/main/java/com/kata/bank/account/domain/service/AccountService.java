71524012e60a6317e0c28a07f84f766bc9ba7612
Hassib <maslah.has@gmail.com>
2022-02-22 17:04:55 +0100

    [ADD] add withdraw from account service

--- src/main/java/com/kata/bank/account/domain/service/AccountService.java
+++ src/main/java/com/kata/bank/account/domain/service/AccountService.java
@@ -2,10 +2,13 @@ package com.kata.bank.account.domain.service;
 
 import com.kata.bank.account.domain.Account;
 import com.kata.bank.account.domain.DepositRequest;
+import com.kata.bank.account.domain.WithdrawRequest;
 
 public interface AccountService {
 
     boolean makeDeposit(DepositRequest depositRequest);
+
+    boolean makeWithdrawal(WithdrawRequest withdrawRequest);
 
     Account getAccount();
 
--- src/main/java/com/kata/bank/account/domain/service/impl/AccountServiceImpl.java
+++ src/main/java/com/kata/bank/account/domain/service/impl/AccountServiceImpl.java
@@ -2,6 +2,7 @@ package com.kata.bank.account.domain.service.impl;
 
 import com.kata.bank.account.domain.Account;
 import com.kata.bank.account.domain.DepositRequest;
+import com.kata.bank.account.domain.WithdrawRequest;
 import com.kata.bank.account.domain.service.AccountService;
 
 public class AccountServiceImpl implements AccountService {
@@ -20,6 +21,24 @@ public class AccountServiceImpl implements AccountService {
         }
         // sum account balance and the amount
         account.setAmount(account.getAmount() + depositRequest.getAmount());
+        // persist the account
+        saveAccount(account);
+
+        return true;
+    }
+
+    @Override
+    public boolean makeWithdrawal(WithdrawRequest withdrawRequest) {
+        Account account = getAccount();
+        if (account == null) {
+            return false;
+        }
+        // check available balance
+        if (account.getAmount() < withdrawRequest.getAmount()) {
+            return false;
+        }
+        // sum account balance and the amount
+        account.setAmount(account.getAmount() - withdrawRequest.getAmount());
         // persist the account
         saveAccount(account);
 
